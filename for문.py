9421ae27aff58947a4e2d7f50b346d60246d3307
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-04-03 19:08:52 +0900

    백준-for문 단계

---
+++ for문.py
@@ -0,0 +1,17 @@
+#1. 구구단(2739)
+dan = int(input())
+for i in range(1, 10):
+    print("%d * %d = %d" %(dan, i, dan*i))
+
+
+    
+    
+#2. A+B-3(10950)
+count = int(input())
+ans = []
+
+for i in range(count):
+    ans.append(sum(map(int,input().split())))
+
+for data in ans:
+    print(data)
