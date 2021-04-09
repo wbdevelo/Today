555ff41998021cd7bfd563fd9b93e5ae6d2daec1
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-04-09 20:34:10 +0900

    for문_단계별

--- python_백준-for문.py
+++ python_백준-for문.py
@@ -67,3 +67,15 @@ import sys
 num = int(input())
 for i in range(1, num+1):
     print("Case #%d: %d" %(i, sum(map(int, sys.stdin.readline().split()))))
+
+    
+    
+    
+    
+#8. A+B-8(11022)
+import sys
+
+num = int(input())
+for i in range(num):
+    a, b = map(int, sys.stdin.readline().split())
+    print("Case #%d: %d + %d = %d" %(i+1, a, b, a+b))
