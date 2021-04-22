e7431a781092e0086773d3736691f02421cd9add
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-04-22 23:13:20 +0900

    Algorithm

--- Greedy.py
+++ Greedy.py
@@ -11,3 +11,23 @@ for i in reversed(coin):
     
 
 print(count)
+
+
+
+#2. 큰 수의 법칙
+n, m ,k = map(int, input().split())
+li = list(map(int, input().split()))
+li.sort(reverse=True)
+sum = 0
+count = 0
+
+for i in range (m):
+    if(count < k):
+        sum += li[0]
+        count += 1
+    else:
+        count = 0
+        sum += li[1]
+        
+
+print(sum)
