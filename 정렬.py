040f3de6c1583fc3cc1a0363de158018b71b1229
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-05-15 22:27:29 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -9,3 +9,17 @@ for i in range(len(cards)):
             cards[j], cards[i] = cards[i], minNum #스와프
 
 print(cards)
+
+
+
+
+
+# 삽입정렬
+cards = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(cards)):
+    for j in range(i, 0, -1):
+        if cards[j] < cards[j-1]:
+            cards[j-1], cards[j] = cards[j], cards[j-1]
+
+print(cards)
