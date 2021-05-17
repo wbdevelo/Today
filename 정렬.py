44f595fa153469402e719bd5e270c8464ce8f5c8
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-05-17 22:50:21 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -23,3 +23,18 @@ for i in range(1, len(cards)):
             cards[j-1], cards[j] = cards[j], cards[j-1]
 
 print(cards)
+
+
+
+
+
+# 계수정렬
+datas = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+count = [0]*(max(datas)+1)
+
+for data in datas:
+    count[data] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=" ")
