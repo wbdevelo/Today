fd4be113c9079e8110d351516d076d1e115b7445
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-05-24 23:41:53 +0900

    Update 정렬.py

--- 정렬.py
+++ 정렬.py
@@ -28,6 +28,29 @@ print(cards)
 
 
 
+# 퀵정렬
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick(array):
+
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+
+    left = [x for x in tail if x <= pivot]
+    right = [x for x in tail if x > pivot]
+
+    return quick(left) + [pivot] + quick(right)
+
+
+print(quick(array))
+
+
+
+
+
 # 계수정렬
 datas = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 count = [0]*(max(datas)+1)
