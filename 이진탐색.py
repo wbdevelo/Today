447777aeb9e7c1c8f556f965ca5655b31f4ffc72
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-05-27 06:09:21 +0900

    Update 이진탐색.py

--- 이진탐색.py
+++ 이진탐색.py
@@ -9,3 +9,32 @@ n, target = input().split()
 array = input().split()
 
 print(sequential(n, array, target))
+
+
+
+
+
+# 이진탐색
+def binary(array, target, start, end):
+
+    if start > end:
+        return None
+    
+    mid = (start + end) // 2
+    if(array[mid] == target):
+        return mid+1
+    else:
+        if(array[mid] > target):
+            return binary(array, target, start, mid-1)
+        else:
+            return binary(array, target, mid+1, end)
+
+
+n, target = map(int, input().split())
+array = list(map(int, input().split()))
+
+result = binary(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result)
