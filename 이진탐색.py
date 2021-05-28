3d274f6a3c983ce610870f064d4ba3c74ad61d4d
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-05-28 22:46:54 +0900

    Update 이진탐색.py

--- 이진탐색.py
+++ 이진탐색.py
@@ -38,3 +38,33 @@ if result == None:
     print("원소가 존재하지 않습니다.")
 else:
     print(result)
+
+    
+    
+    
+    
+# 부품찾기_이진
+n = int(input())
+array = list(map(int, input().split()))
+m = int(input())
+targets = list(map(int, input().split()))
+
+def find(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if target == array[mid]:
+            return "yes"
+        elif target < array[mid]:
+            end = mid-1
+        else:
+            start = mid+1
+
+    return None
+
+for target in targets:
+    result = find(array, target, 0, n-1)
+    if result == None:
+        print("no", end=' ')
+    else:
+        print("yes", end=' ')
