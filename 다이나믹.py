e98f805f9c6b83a2a212f1ca863880b8ff9ac8d9
Minseong Seo <70123016+wbdevelo@users.noreply.github.com>
2021-06-06 15:52:29 +0900

    Update 다이나믹.py

--- 다이나믹.py
+++ 다이나믹.py
@@ -14,3 +14,18 @@ for i in range(2, x+1):
 
 print(d[x])
 
+
+
+
+
+#2. 개미전사
+n = int(input())
+k = list(map(int, input().split()))
+d = [0]*30000
+
+d[0] = k[0]
+d[1] = max(k[0], k[1])
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2] + k[i])
+
+print(d[n-1])
